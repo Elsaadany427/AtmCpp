7a8a38d7bb376b8771a214da5da12ed9931da94b
Elsaadany <57992732+Elsaadany427@users.noreply.github.com>
2021-07-14 19:40:43 +0200

    Update Atm.cpp

--- Atm.cpp
+++ Atm.cpp
@@ -10,7 +10,7 @@ TASKS:		Create an account that has the ability to
 				4) Make deposits into accounts
 				5) Make withdrawals from accounts
 
-AUTHOR:		Heba
+AUTHOR:		Hatem Bassem
 
 INSTRUCTIONS:	
 			Enter pin number '1234'
